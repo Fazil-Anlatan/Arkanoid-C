0568437bfe5de93c49a686987d1822f5f229a929
mlopezpuigitba <mlopezpuig@itba.edu.ar>
2026-08-12 23:51:43 -0300

    random ball start (without english coments )

--- initialization_structures.c
+++ initialization_structures.c
@@ -1,5 +1,9 @@
 #include "established_parameters.h"
 #include "initialization_structures.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
 //-------------GLOBAL VAR DECLARATIONS------------
 Ball ball;
 Brick brick[BRICK_ROWS][BRICK_COLUMNS];
@@ -15,12 +19,23 @@ void gameInicialization() {
    gameInicialization_bricks();
 }
 
-void gameInicialization_ball() {
-    
+void gameInicialization_ball() { //isn't finished yed
     ball.x = SCREEN_WIDTH / 2;
     ball.y = SCREEN_HEIGHT / 2;
-    ball.vx = 0.5f;
-    ball.vy = 1.0f;
+
+    ball.vx = ((time(NULL) % 8) + 1) / 10.0; //velocidad en x entre 0.1 y 0.9
+  
+    //el vector velocidad debe tener un modulo de 1.5
+    //se calcula vy usando el teorema de Pitagoras: vy = sqrt(1.5^2 - vx^2)
+    ball.vy = sqrt(pow(1.5, 2) - pow(ball.vx, 2)); //la norma del vec velocidad va a tener que ser un define por mala practica
+    
+    //direcciones aleatorias dependiendo de time(NULL)
+    if (time(NULL) % 2 == 1) {
+        ball.vx *= -1;
+    }
+    if (time(NULL) % 3 == 1) {
+        ball.vy *= -1;
+    }
 }
 
 void gameInicialization_paddle() {
