127c13cdda1b84f2c542069eb05395d7b7adcf8a
jtossounian <jtossounian@itba.edu.ar>
2026-08-21 15:44:54 -0300

    Update initialization_structures.h
    Added capsules and changed brick structure

--- initialization_structures.h
+++ initialization_structures.h
@@ -5,6 +5,7 @@ void gameInicialization(); //Divided into 3 functions: ball, paddle and bricks
 void gameInicialization_ball();
 void gameInicialization_paddle();
 void gameInicialization_bricks();
+void gameInicialization_capsules();
 //-------------STRUCTURES-----------
 typedef struct{ //ball
     float x, y;
@@ -13,10 +14,18 @@ typedef struct{ //ball
 
 typedef struct{ //bricks
     int health;
+    int contains_capsule;
 }Brick;
 
 typedef struct{ //paddle
     int x;
     int size;
 }Paddle;
+
+typedef struct{
+    int x;
+    float vy, y;
+    int type;
+}Capsule;
+
 #endif
