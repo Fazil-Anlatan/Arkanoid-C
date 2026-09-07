0aaa946a0c54814f82ab177db7568fff2e9fc625
jtossounian <jtossounian@itba.edu.ar>
2026-09-07 17:07:02 -0300

    Corrected reset_game function

--- game_data.h
+++ game_data.h
@@ -8,5 +8,5 @@ void level_up(Game *game, int c);
 int advance_score_calc(Game *game, int brick_y);
 int  load_scores(ScoreEntry top[], int max);
 void save_score(Game *game); // adds one line to scores.txt every time a game ends
-void reset_game();
+void reset_game(Game *game);
 #endif
