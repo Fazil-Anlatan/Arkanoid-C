7dd044fba612c6bb900b8772abe634876fb10e24
jtossounian <jtossounian@itba.edu.ar>
2026-09-07 16:56:52 -0300

    Changed a brick row constant so that we can work with initial and maximum. Also fixes segfault

--- established_parameters.h
+++ established_parameters.h
@@ -13,7 +13,7 @@
     #define MAX_BALLS 3
     #define MAX_ANGLE 70.0f * 3.14159265f / 180.0f
 
-    #define BRICK_ROWS 4 // We should make it that the number of rows increase with the level, but 1 is the initial value.
+    #define INITIAL_BRICK_ROWS 4
     #define MAX_BRICK_ROWS 10
     #define BRICK_COLUMNS 8
   
